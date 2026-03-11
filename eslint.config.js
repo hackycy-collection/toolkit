0cdd0db6ec77e838ef9e30602a51ecfaf4b5e25c
Leo Yang <qa894178522@qq.com>
2026-03-11 21:03:19 +0800

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ export default antfu(
   {
     type: 'lib',
     pnpm: true,
+    ignores: ['./playground/package.json'],
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'off',
     },
